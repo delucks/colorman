00ae2229d6cc4f4730baecb7d0984d532a9374ab
delucks <jamie.delucks@gmail.com>
2015-05-18 14:42:42 -0400

    Separated out dotshare stuff so I can use it in a future script

--- py/dotshare.py
+++ py/dotshare.py
@@ -1,4 +1,3 @@
-import argparse
 import urllib2
 import getpass
 from color_common import reformat
@@ -6,18 +5,17 @@ from color_common import reformat
 # grab a termcolors dot from dotshare.it, reformat and add to your colors dir
 # usage: python2 dotshare.py {dotID}
 
-# quick hack to make sure the path is formatted right
-COLORS_DIR="/home/{user}/dotfiles/colors".format(user=getpass.getuser())
+def dotgrab(dot_id, location):
+    USER_AGENT = "https://github.com/delucks X colors manager"
+    headers = {'User-Agent': USER_AGENT} 
+    baseurl="http://dotshare.it/dots/{dotid}/0/raw".format(dotid=dot_id) 
+    r = urllib2.Request(baseurl, None, headers)
+    reformat(urllib2.urlopen(r).read(),location,dot_id)
 
-p = argparse.ArgumentParser(description="grab a dot from http://dotshare.it/category/terms/colors/, put it in your colors dir")
-p.add_argument("dot", help="the dotshare ID of the file you want. e.g. http://dotshare.it/dots/87/ would be 87")
-p.add_argument("-l","--location",help="set alternate location for the downloaded/processed dot",default=COLORS_DIR)
-args = p.parse_args()
-
-USER_AGENT = "https://github.com/delucks X colors manager"
-headers = {'User-Agent': USER_AGENT} 
-baseurl="http://dotshare.it/dots/{dotid}/0/raw".format(dotid=args.dot) 
-
-r = urllib2.Request(baseurl, None, headers)
-
-reformat(urllib2.urlopen(r).read(),args.location,args.dot)
+if (__name__ == '__main__'):
+    import argparse
+    p = argparse.ArgumentParser(description="grab a dot from http://dotshare.it/category/terms/colors/, put it in your colors dir")
+    p.add_argument("dot", help="the dotshare ID of the file you want. e.g. http://dotshare.it/dots/87/ would be 87")
+    p.add_argument("-l","--location",help="set alternate location for the downloaded/processed dot",default=COLORS_DIR)
+    args = p.parse_args()
+    dotgrab(args.dot, args.location)
